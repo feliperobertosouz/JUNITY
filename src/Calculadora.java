1a28ee5fe9c90f9cf8cb0286cf83362451a31d7a
Felipe Roberto <56833987+feliperobertosouz@users.noreply.github.com>
2023-10-25 15:45:22 -0300

    feat: added doc strings to Calculadora.java

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,17 @@
+/**
+ *
+ * The Calculadora class is a class responsible to make basic math operations
+ * Calculadora not have a builder, because it is not necessary
+ * @author Felipe Roberto
+ */
 public class Calculadora {
 
+    /**
+     * soma made a sum of two integers
+     * @param a number one to sum
+     * @param b number two to sum
+     * @return int , the result of sum of a and b
+     */
 
     //Recebe 2 inteiros e retorna a soma deles
     public int soma(int a, int b){
@@ -7,14 +19,35 @@ public class Calculadora {
     }
 
     //Recebe 2 inteiros e retorna a subtracao deles
+
+    /**
+     * subtracao made a subtraction of two integers
+     * @param a number one to subtraction
+     * @param b number two to subtracion
+     * @return int , the result of subtraction of a and b
+     */
     public int subtracao(int a, int b){
         return a - b;
     }
 
     //Recebe 2 inteiros e retorna a multiplicacao deles
+
+    /**
+     * multiplicacao made a multiplication of two integers
+     * @param a number one to multiplication
+     * @param b number two to multiplication
+     * @return int, the result of multiplication of a and b
+     */
     public int multiplicacao(int a, int b){
         return a * b;
     }
+
+    /**
+     * divisao made a division of two integers
+     * @param a number one to division
+     * @param b number two to division
+     * @return int, the result of division of a and b
+     */
 
     //Recebe 2 inteiros e retorna a divisao deles
     public int divisao(int a, int b){
